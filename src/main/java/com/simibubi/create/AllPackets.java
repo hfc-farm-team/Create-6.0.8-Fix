8fcef60a4aefb732173f054e163bcd2f95840d31
Fugit-5414 <1322594375@qq.com>
2026-08-13 05:24:26 +0800

    Bump version to Fix.4

--- src/main/java/com/simibubi/create/AllPackets.java
+++ src/main/java/com/simibubi/create/AllPackets.java
@@ -272,7 +272,7 @@ public enum AllPackets {
 	}
 
 	public static final ResourceLocation CHANNEL_NAME = Create.asResource("main");
-	public static final int NETWORK_VERSION = 608003;  // 6.0.8 第三次修复
+	public static final int NETWORK_VERSION = 608004;  // 6.0.8 第4次修复
 	public static final String NETWORK_VERSION_STR = String.valueOf(NETWORK_VERSION);
 	private static SimpleChannel channel;
 
